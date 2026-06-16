63f6ad2a4736eb4e6b1d2b7b70e7ee6433003ca5
Liron Atar <lironatar94@gmail.com>
2026-06-16 21:32:34 +0300

    Import path module in api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,6 +3,7 @@ const db = require('../database');
 const { authenticateToken } = require('./auth');
 const os = require('os');
 const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
